ea0324f4601f9c7fca08ee09c0fdb4445b1958ca
Wesley Harris <wesley@windupbutton.com>
2019-10-29 15:16:44 +0100

    Fix ValuesClause omitting null values

--- src/WindupButton.Roscoe/Infrastructure/DbCommandBuilder.cs
+++ src/WindupButton.Roscoe/Infrastructure/DbCommandBuilder.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2019 Windup Button
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //    http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -45,7 +45,7 @@ namespace WindupButton.Roscoe.Infrastructure
         public string Sql => stringWriter.GetStringBuilder().ToString();
         public IEnumerable<DbParameter> Parameters => parameters;
 
-        public DbParameter AddParameter(object value)
+        public DbParameter AddParameter(object? value)
         {
             var parameter = parameterFactory.Create(value);
 
