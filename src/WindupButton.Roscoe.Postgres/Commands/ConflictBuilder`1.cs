ec2c8e92c93f4cf5911bb0bf4a12f150309180f9
Wesley Harris <wesmharris@gmail.com>
2020-01-10 10:41:12 +0100

    Fix postgres on conflict do update

--- src/WindupButton.Roscoe.Postgres/Commands/ConflictBuilder`1.cs
+++ src/WindupButton.Roscoe.Postgres/Commands/ConflictBuilder`1.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2019 Windup Button
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //    http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,7 +35,7 @@ namespace WindupButton.Roscoe.Postgres.Commands
             return Update(null, updateAction);
         }
 
-        public IWrapper<T> Update(DbBool whereClause, Action<ConflictUpdateBuilder> updateAction)
+        public IWrapper<T> Update(DbBool? whereClause, Action<ConflictUpdateBuilder> updateAction)
         {
             Check.IsNotNull(updateAction, nameof(updateAction));
 
